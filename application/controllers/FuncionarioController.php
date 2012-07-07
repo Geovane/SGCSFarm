a88d6d189ca261589fb23e44fdbd7f9b94e9c75d
geovane <geovane@codigointerno.com>
2012-07-06 23:02:33 -0300

    Meus dados terminado e organizado em abas.

--- application/controllers/FuncionarioController.php
+++ application/controllers/FuncionarioController.php
@@ -137,53 +137,6 @@ class FuncionarioController extends Zend_Controller_Action
 
     }
 
-
-     public function imagemAction(){
-
-
-
-            if ( $this->_request->isPost() )
-            {
-
-              $idFunc = $this->funcLogado->idfuncionario;
-              $permitido = array('jpg', 'jpeg', 'png', 'gif');
-              $file = $this->uploadFiles('images/fotosFunc/', true , $permitido, $idFunc );
-
-              if ( !empty ($file) ){
-
-
-                //Redimensionando imagem
-
-                 $data1 = array(
-                        'valor' => '/images/fotosFunc/' . $file[0]
-                 );
-
-                require('../public/scripts/wideimage-11.02.19-full/lib/WideImage.php');
-                $image = wideImage::load('../public' . $data1['valor']);
-                //tamanho da imagem
-                $image = $image->resize(102, 127);
-                $image->saveToFile('../public' . $data1['valor']);
-
-                $data = array(
-                    'foto' => '/images/fotosFunc/'.$file[0]
-                );
-
-                $where = $this->funcionario->getAdapter()->quoteInto('idfuncionario = ?', (int) $idFunc);
-                $this->funcionario->update($data, $where);
-
-                $this->_redirect('funcionario/index/flag/6');
-
-              }else{
-
-                 $this->_redirect('funcionario/index/flag/5');
-
-              }
-               
-                
-            }
-
-    }
-
     public function deleteAction(){
 
         $func_id = $this->_getParam('id');
@@ -250,71 +203,5 @@ class FuncionarioController extends Zend_Controller_Action
 
         return true;
     }
-
-     private function uploadFiles($uploadDir = null, $renameFile = true , $allowedExtensions = array(), $id = "x")
-            {
-            //Caso o diretorio não exista , cria o diretorio para o destino
-            if(!file_exists($uploadDir))
-                    mkdir($uploadDir, 0777, true);
-
-            $adapter = new Zend_File_Transfer_Adapter_Http();
-            $adapter->setDestination($uploadDir);
-
-            $files = $adapter->getFileInfo();
-
-            //Pega o nome dos campos
-            $fields = array_keys($files);
-
-            //Variavel contendo tipos
-            $i= 0;
-
-            foreach($files as $info)
-            {
-                $filename = strtolower($info['name']) ;
-                $exts = split("[/\\.]", $filename) ;
-                $n = count($exts)-1;
-                $exts = $exts[$n];
-
-
-
-                    $ext = $exts;
-                    if($info['name'] != '')
-                    {
-                            //verifica se foi passado os tipos de arquivos permitidos no parametro
-                            if(count($allowedExtensions) > 0)
-                            {
-                                    //verifica se a extenção enviada é uma extenção valida
-                                        if(!array_keys($allowedExtensions, $ext))
-                                        {
-                                        return null;
-                                        }
-                            }
-                    //Renomeia o Arquivo
-                    //Caso o arquivo tenha sido carregado, insere o nome do arquivo ao array
-                    //Ele tendo sido sobescrito ou não
-
-                    if($renameFile)
-                    {
-                            $fileName = $id."_".time().".".$ext;
-                            $adapter->addFilter('Rename', array('target' => $uploadDir.DIRECTORY_SEPARATOR.$fileName,'overwrite' => true));
-                            $filesUploaded[] = $fileName;
-                    }
-                    else
-                        $filesUploaed[] = $info['name'];
-
-                        $adapter->receive($info['name']);
-            }
-        $i++;
-       }
-
-       return $filesUploaded;
-     }
-
-     public function meusdadosAction(){
-
-        $dados = $this->funcionario->find($this->funcLogado->idfuncionario);
-        $this->view->dados = $dados[0];
-       
-     }
 
 }
