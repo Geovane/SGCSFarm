4b5f66ba34ff8b705650a9a8d7e69c0be4dd2f4a
BrunoPereiraSantos <bruno.ps@live.com>
2012-07-11 20:11:47 -0300

    Comentarios adicionados no Controller Projetos, Tarefas e em todos os Models

--- application/models/DbTable/Estado.php
+++ application/models/DbTable/Estado.php
@@ -1,9 +1,29 @@
 <?php
-
+/**
+ * Esta classe tem como objetivo efetuar a escolha da tabela 'estado'
+ * no banco de dados.
+ * 
+ * @author SoftFarm
+ * @version 0.1
+ * @access public
+ * @copyright Copyright © 2012, SoftFarm.
+ * 
+ */
 class Model_DbTable_Estado extends Zend_Db_Table_Abstract
 {
 
+    /**
+    * Variavel que recebe o nome da tabela a ser acessada.
+    * @access private
+    * @name $_name
+    */
     protected $_name = 'estado';
+    
+    /**
+    * Variavel que recebe a chave primaria da tabela a ser acessada.
+    * @access private
+    * @name $_primary
+    */
     protected $_primary = 'idestado';
 
 
--- application/models/DbTable/estadoProj.php
+++ application/models/DbTable/estadoProj.php
@@ -1,11 +1,40 @@
 <?php
-
+/**
+ * Esta classe tem como objetivo efetuar a escolha da tabela 'estado_projeto'
+ * no banco de dados.
+ * 
+ * @author SoftFarm
+ * @version 0.1
+ * @access public
+ * @copyright Copyright © 2012, SoftFarm.
+ * 
+ */
 class Model_DbTable_EstadoProj extends Zend_Db_Table_Abstract
 {
+    /**
+    * Variavel que recebe o nome da tabela a ser acessada.
+    * @access private
+    * @name $_name
+    */
     protected $_name = 'estado_projeto';
+    
+    /**
+    * Variavel que recebe o nome da tabela a ser acessada.
+    * @access private
+    * @name $_primary
+    */
     protected $_primary = 'nomeProj';
     
-    //busca o estado de um projeto pelo id.
+    //
+    
+    /**
+     * Funcao que busca o estado de um projeto pelo id.
+     * 
+     * @access public 
+     * @param int $idProj contendo o id do projeto que se deseja o estado.
+     * @return String[] contendo o estado atual daquele projeto com $idProj.
+     * 
+     */
      public function buscaEstadoProj($idProj)
     {
         $select = $this->select()
